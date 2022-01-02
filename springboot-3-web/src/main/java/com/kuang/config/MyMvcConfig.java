8b2981ddcf182f951b7c4bf48a708571e78a9439
lh <1178285946@qq.com>
2022-01-02 21:45:55 +0800

    员工管理系统

---
+++ springboot-3-web/src/main/java/com/kuang/config/MyMvcConfig.java
@@ -0,0 +1,36 @@
+package com.kuang.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+//扩展Mvc
+//如果你想diy一些定制化的功能，只要写这个组件，然后把他交给springboot，就会自动装配
+//扩展 springMvc dispatchservlet
+@Configuration
+public class MyMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginHandlerInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/index.html","/","/user/login","/css/**","/img/**","/js/**");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/index.html").setViewName("index");
+        registry.addViewController("/main.html").setViewName("dashboard");
+    }
+
+    //自定义的国际化组件生效
+    @Bean
+    public LocaleResolver localeResolver(){
+        return new MyLocaleResolver();
+    }
+
+
+}
